a13f6d13882f1a06804bbbe98ab2ea1ee1a08ca7
JohnDoe2576 <63945526+JohnDoe2576@users.noreply.github.com>
2020-04-26 00:20:16 +0530

    Add files via upload

--- PostProcess.m
+++ PostProcess.m
@@ -10,12 +10,15 @@ hold on; box on; grid on;
 plot( t, y( :, 1 ), 'Color', clr.muted_blue, ...
         'LineStyle', '-', 'LineWidth', 0.5 );
 xlabel( 't [s]' ); ylabel( 'U / Y' ); title( 'Input-Output' );
+legend( 'Input, U', 'Output, Y' );
+set( legend, 'Location', 'South' );
+set( legend, 'Orientation', 'Horizontal');
 xlim( [ min( t ) max( t ) ] ); ylim( [ -2 8 ] );
-xticks = 0:100:300; yticks = -2:3:8;
+xticks = 0:300:900; yticks = -2:3:8;
 FigureAesthetics( fh, 'Times New Roman', 12, clr );
 
-% exportgraphics( fh, 'fig\png\MagLevData.png', 'Resolution', 1200 );
-% exportgraphics( fh, 'fig\eps\MagLevData.eps');
+% exportgraphics( fh, 'fig\png\MagLevTrainingData.png', 'Resolution', 1200 );
+% exportgraphics( fh, 'fig\eps\MagLevTrainingData.eps');
 % exportgraphics( fh, 'fig\png\MagLevTestDataTR.png', 'Resolution', 1200 );
 % exportgraphics( fh, 'fig\eps\MagLevTestDataTR.eps');
 % exportgraphics( fh, 'fig\png\MagLevTestDataSS.png', 'Resolution', 1200 );
